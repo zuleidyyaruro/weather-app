348e0bbe0b970cee765460df17d485c3fab930f3
Zuleidy Yaruro <zvyaruro@gmail.com>
2021-05-09 17:57:45 -0500

    edit background

--- src/helpers/background.js
+++ src/helpers/background.js
@@ -12,6 +12,10 @@ export const setBackgroundImage = (op) => {
         case "Drizzle":
             background="https://cdn.pixabay.com/photo/2017/08/05/15/19/puddle-2584378_960_720.jpg"
             break;
+        case "Clouds":
+            background="https://images.pexels.com/photos/2678435/pexels-photo-2678435.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            break;
+    
 
     }
     return background;
